bb06b9b335e459faed5021e3690479556c80ddfd
Rahul Vikram <75101301+rahulvikram@users.noreply.github.com>
2025-12-26 02:06:42 -0800

    feat: installs and uses MagicUI bg

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,7 @@ import { Welcome } from "../welcome/welcome";
 import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
+import { DotPattern } from "../components/ui/dot-pattern";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,7 +15,8 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center gap-16 min-h-screen">
+      <div className="relative h-[500px] w-full overflow-hidden flex flex-col items-center justify-center gap-16 min-h-screen">
+        {/* <DotPattern width={10} height={10} className="opacity-80"/> */}
         <Header />
         <SignedIn>
           <Welcome />
